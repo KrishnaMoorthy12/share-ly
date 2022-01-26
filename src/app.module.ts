f98550abf35fc14b0a3e4bf755369f15675d5f66
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-01-27 01:00:18 +0530

    feat: added morgan request logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MorganInterceptor, MorganModule } from 'nest-morgan';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { morganDevFormat } from './tools/request-logger';
 
 @Module({
-  imports: [],
+  imports: [MorganModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'APP_INTERCEPTOR',
+      useClass: MorganInterceptor(morganDevFormat),
+    },
+  ],
 })
 export class AppModule {}
