9b74de63bbe9d8461bc404266411192b9f5cb368
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-01-27 10:58:29 +0530

    fix: empty dto in docs & added examples

--- src/modules/user/dto/change-key.input.dto.ts
+++ src/modules/user/dto/change-key.input.dto.ts
@@ -1,9 +1,13 @@
-import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsMongoId, IsString } from 'class-validator';
+import { ObjectID as ObjectIdType } from 'typeorm';
 
 export class ChangeKeyInputDto {
+  @ApiProperty({ example: 'WareWolf#254' })
   @IsString()
   readonly key: string;
 
-  @IsString()
-  readonly userId: string;
+  @ApiProperty({ type: String, format: 'hexadecimal', example: '507f1f77bcf86cd799439011' })
+  @IsMongoId()
+  readonly userId: ObjectIdType;
 }
--- src/modules/user/dto/create-user.input.dto.ts
+++ src/modules/user/dto/create-user.input.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateUserInputDto {
+  @ApiProperty({ example: 'Krish' })
   @IsString()
   readonly name: string;
 
+  @ApiProperty({ example: 'akrishnamoorthy007@email.com' })
   @IsString()
   readonly email: string;
 
+  @ApiProperty({ example: 'WareWolf#254' })
   @IsString()
   readonly key: string;
 }
